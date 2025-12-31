aef9f36ae131f05199e66691cbc8f09f0f4210d9
Alfitrm <grackrev@gmail.com>
2025-12-31 20:39:19 +0700

    feat: add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "../styles/globals.css";
 
 const geistSans = Geist({
@@ -76,6 +77,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <CustomCursor />
+        <SpeedInsights />
         {children}
       </body>
     </html>
